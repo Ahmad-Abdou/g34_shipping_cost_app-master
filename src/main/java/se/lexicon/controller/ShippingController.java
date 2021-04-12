e966b4966fffab46c9e5b3caa4b2d82b80539942
Ahmad-Abdou <kyoub1996@gmail.com>
2021-04-12 10:41:58 +0200

    Done the front End part

--- src/main/java/se/lexicon/controller/ShippingController.java
+++ src/main/java/se/lexicon/controller/ShippingController.java
@@ -1,20 +1,34 @@
 package se.lexicon.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
 public class ShippingController {
 
 
-    @GetMapping("/form")
+    @GetMapping("/")
     public String addBoxForm(){
-
         return "AddBoxForm";
     }
-
-    @GetMapping("show/list")
+    @GetMapping("/show/list")
     public String showBoxList(){
-
         return "ShowBoxList";
     }
+    @GetMapping("/form")
+    public String showingBox(Model model){
+
+        return "ShowBoxList";
+
+    }
+    @PostMapping("/add/box")
+    public String adding(){
+
+        return "redirect:/ShowBoxList";
+    }
+
+
 }
 
--- src/main/java/se/lexicon/repositroy/BoxRepository.java
+++ src/main/java/se/lexicon/repositroy/BoxRepository.java
@@ -3,5 +3,7 @@ package se.lexicon.repositroy;
 import org.springframework.data.repository.CrudRepository;
 import se.lexicon.entity.Box;
 
-public interface BoxRepository extends CrudRepository<Integer, Box> {
+public interface BoxRepository extends CrudRepository< Box ,Integer> {
+
+
 }
