f72e19afa36f8917fd9d27347b1b67fe696ec1a5
Ahmad-Abdou <kyoub1996@gmail.com>
2021-04-12 09:44:22 +0200

    SpringBoot Application

--- src/main/java/se/lexicon/controller/ShippingController.java
+++ src/main/java/se/lexicon/controller/ShippingController.java
@@ -1,4 +1,20 @@
 package se.lexicon.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class ShippingController {
+
+
+    @GetMapping("/form")
+    public String addBoxForm(){
+
+        return "AddBoxForm";
+    }
+
+    @GetMapping("show/list")
+    public String showBoxList(){
+
+        return "ShowBoxList";
+    }
 }
+
